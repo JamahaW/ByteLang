3618197dea6393fc055a5e33b78dab64a8dcd62d
JamahaW <pret2titio1n@gmail.com>
2024-07-01 01:11:23 +0300

    cleanup tools.Filetool ref

--- src/bytelang/tools.py
+++ src/bytelang/tools.py
@@ -6,54 +6,33 @@ from typing import Iterable
 class FileTool:
     """Обёртка для работы с файлами"""
 
-    @staticmethod
-    def __forFileExecute(filepath: str, mode: str, func):
-        """
-        Выполнить `func` для файла
-        :param func: `lambda f`: ...
-        :return: `ret = func(file)`
-        """
-        with open(filepath, mode) as file:
-            ret = func(file)
-        return ret
-
-    @classmethod
-    def __fileRead(cls, filepath: str, mode: str) -> str | bytes:
-        """
-        Прочесть файл с режимом `mode`
-        :return: `file.read()`
-        """
-        return cls.__forFileExecute(filepath, mode, lambda file: file.read())
-
-    @classmethod
-    def __fileSave(cls, filepath: str, mode: str, _data: str | bytes):
-        """
-        Сохранить файл с режимом `mode`
-        """
-        cls.__forFileExecute(filepath, mode, lambda file: file.write(_data))
-
     @classmethod
     def read(cls, filepath: str) -> str:
-        return cls.__fileRead(filepath, "r")
+        with open(filepath, "rt") as f:
+            return f.read()
 
     @classmethod
-    def readBinary(cls, filepath: str) -> bytes:
-        return cls.__fileRead(filepath, "rb")
-
-    @classmethod
-    def save(cls, filepath: str, _data: str):
-        cls.__fileSave(filepath, "w", _data)
-
-    @classmethod
-    def saveBinary(cls, filepath: str, _data: bytes):
-        cls.__fileSave(filepath, "wb", _data)
+    def readBytes(cls, filepath: str) -> bytes:
+        with open(filepath, "rb") as f:
+            return f.read()
 
     @classmethod
     def readJSON(cls, filepath: str) -> dict | list:
-        return cls.__forFileExecute(filepath, "r", lambda file: json.load(file))
+        with open(filepath) as f:
+            return json.load(f)
 
     @classmethod
-    def getFileNamesByExt(cls, folder: str, extension: str) -> tuple[str]:
+    def save(cls, filepath: str, _data: str):
+        with open(filepath, "wt") as f:
+            f.write(_data)
+
+    @classmethod
+    def saveBinary(cls, filepath: str, _data: bytes):
+        with open(filepath, "wb") as f:
+            f.write(_data)
+
+    @classmethod
+    def getFileNamesByExt(cls, folder: str, extension: str) -> tuple[str, ...]:
         return tuple(file.stem for file in Path(folder).glob(f"*.{extension}"))
 
 
@@ -71,3 +50,6 @@ class ReprTool:
     def headed(name: str, i: Iterable, *, length: int = 120, fill: str = "-") -> str:
         return f"{f' <<< {name} >>> ':{fill}^{length}}\n{ReprTool.column(i)}\n"
 
+
+if __name__ == '__main__':
+    print(FileTool.readJSON("../../data/environments/test_env.json"))
