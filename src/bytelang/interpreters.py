659d79efa5bfce353246ffccd0a083f70a7ed61d
JamahaW <pret2titio1n@gmail.com>
2024-07-05 21:31:26 +0300

    cleanup

--- src/bytelang/interpreters.py
+++ src/bytelang/interpreters.py
@@ -28,27 +28,45 @@ class Interpreter:
         self.__program: Optional[bytearray] = None
 
     def stackPushPrimitive(self, primitive: PrimitiveType, value: int | float) -> None:
+        """Записать значение примитивного типа в стек"""
         self.__stack.extend((primitive.packer.pack(value)))
 
     def stackPopPrimitive(self, primitive: PrimitiveType) -> int | float:
+        """Получить значение примитивного типа из стека"""
         b = bytearray(self.__stack.pop() for _ in range(primitive.size))
         return primitive.packer.unpack(b)[0]
 
+    def ipVariableStackPop(self, primitive: PrimitiveType) -> None:
+        """Записать значение примитивного типа из стека в переменную по IP"""
+        self.addressWritePrimitive(self.ipReadHeapPointer(), primitive, self.stackPopPrimitive(primitive))
+
+    def ipStackPush(self, primitive: PrimitiveType) -> None:
+        """Отправить в стек значение примитивного типа по IP"""
+        self.stackPushPrimitive(primitive, self.ipReadPrimitive(primitive))
+
     def addressWritePrimitive(self, address: int, primitive: PrimitiveType, value: int | float) -> None:
+        """Запись примитивный тип по адресу"""
         primitive.packer.pack_into(self.__program, address, value)
 
     def addressReadPrimitive(self, address: int, primitive: PrimitiveType) -> int | float:
+        """Считать примитивный тип по адресу"""
         return primitive.packer.unpack_from(self.__program, address)[0]
 
+    def ipReadVariable(self, primitive: PrimitiveType) -> int | float:
+        return self.addressReadPrimitive(self.ipReadHeapPointer(), primitive)
+
     def ipReadPrimitive(self, primitive: PrimitiveType) -> int | float:
+        """Считать значение примитивного типа по IP"""
         p = self.__program_pointer
         self.__program_pointer += primitive.size
         return self.addressReadPrimitive(p, primitive)
 
     def ipReadInstructionIndex(self) -> int:
+        """Получить индекс инструкции по IP"""
         return self.ipReadPrimitive(self.__primitive_instruction_index)
 
     def ipReadHeapPointer(self) -> int:
+        """Получить указатель на кучу по IP"""
         return self.ipReadPrimitive(self.__primitive_heap_pointer)
 
     def setExitCode(self, code: int) -> None:
@@ -89,54 +107,49 @@ class TestInterpreter(STDInterpreter):
 
     def __init__(self, env: Environment, primitives: PrimitivesRegistry):
         super().__init__(env, primitives, (
-            self.__ins_exit,
-            self.__ins_print,
-            self.__ins_print8,
-            self.__ins_inc,
-            self.__ins_write,
-            self.__ins__push32,
-            self.__ins__pop32,
-            self.__ins__pop16,
-            self.__ins__pop8,
+            self.__exit,
+            self.__print,
+            self.__print8,
+            self.__inc,
+            self.__write,
+            self.__push32,
+            self.__pop32,
+            self.__pop16,
+            self.__pop8,
         ))
 
-    def __ins_exit(self) -> None:
+    @staticmethod
+    def stdoutWrite(value: str) -> None:
+        print(value, end="")
+
+    def __exit(self) -> None:
         self.setExitCode(self.ipReadPrimitive(self.u8))
 
-    def __ins_print(self) -> None:
-        address = self.ipReadHeapPointer()
-        value = self.addressReadPrimitive(address, self.u32)
-        print(f"|> {value}")
+    def IPPrint(self, primitive: PrimitiveType) -> None:
+        self.stdoutWrite(f"|> {self.ipReadVariable(primitive)}\n")
 
-    def __ins_print8(self) -> None:
-        address = self.ipReadHeapPointer()
-        value = self.addressReadPrimitive(address, self.u8)
-        print(f"|> {value:02X}")
+    def __print(self) -> None:
+        self.IPPrint(self.u32)
 
-    def __ins_inc(self) -> None:
+    def __print8(self) -> None:
+        self.IPPrint(self.u8)
+
+    def __inc(self) -> None:
         address = self.ipReadHeapPointer()
         value = self.addressReadPrimitive(address, self.i16)
         self.addressWritePrimitive(address, self.i16, value + 1)
 
-    def __ins_write(self) -> None:
-        value = self.ipReadPrimitive(self.u8)
-        print(chr(value), end="")
+    def __write(self) -> None:
+        self.stdoutWrite(chr(self.ipReadPrimitive(self.u8)))
 
-    def __ins__push32(self) -> None:
-        value = self.ipReadPrimitive(self.u32)
-        self.stackPushPrimitive(self.u32, value)
+    def __push32(self) -> None:
+        self.ipStackPush(self.u32)
 
-    def __ins__pop32(self) -> None:
-        addr = self.ipReadHeapPointer()
-        value = self.stackPopPrimitive(self.u32)
-        self.addressWritePrimitive(addr, self.u32, value)
+    def __pop32(self) -> None:
+        self.ipVariableStackPop(self.u32)
 
-    def __ins__pop16(self) -> None:
-        addr = self.ipReadHeapPointer()
-        value = self.stackPopPrimitive(self.u16)
-        self.addressWritePrimitive(addr, self.u16, value)
+    def __pop16(self) -> None:
+        self.ipVariableStackPop(self.u16)
 
-    def __ins__pop8(self) -> None:
-        addr = self.ipReadHeapPointer()
-        value = self.stackPopPrimitive(self.u8)
-        self.addressWritePrimitive(addr, self.u8, value)
+    def __pop8(self) -> None:
+        self.ipVariableStackPop(self.u8)
